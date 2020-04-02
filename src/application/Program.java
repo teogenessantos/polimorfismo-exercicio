82586f52991aa71a24451f0a847e87e951d61223
Teogenes <teogenes.santos@gmail.com>
2020-04-02 17:59:39 -0300

    Implementacao da Classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,60 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		List<Product> listProduct = new ArrayList<>();
+		System.out.print("Enter the number of products: ");
+		Integer qtdProducts = sc.nextInt();
+		Character typeProduct;
+		String name;
+		Double price;
+		
+		for (int i = 1; i <= qtdProducts; i++) {
+			sc.nextLine();
+			System.out.print("Product #"+i+" data:");
+			System.out.print("Common, used or imported (c/u/i)?");
+			typeProduct = sc.next().charAt(0);
+			System.out.print("Name: ");
+			name = sc.nextLine();
+			System.out.print("Name: ");
+			price = sc.nextDouble();
+			if(typeProduct == 'i') {
+				System.out.print("Customs fee: ");
+				Double customesFee = sc.nextDouble();
+				listProduct.add(new ImportedProduct(name,price,customesFee));
+			}else if(typeProduct == 'i') {
+				System.out.print("Manufacture date (DD/MM/YYYY): ");
+				Date manufactureDate = sdf.parse(sc.next());
+				listProduct.add(new UsedProduct(name, price, manufactureDate));
+			}else {
+				listProduct.add(new Product(name, price));
+			}
+		}
+		System.out.print("PRICE TAGS:");
+		for (Product product : listProduct) {
+			if(product instanceof ImportedProduct) {
+				System.out.printf("%s $ %.2f (Customs fee: $ %.2f%n)",product.getName(),product.getPrice(),((ImportedProduct) product).getCustomesFee());
+			}else if(product instanceof UsedProduct){
+				System.out.printf("%s (Used) $ %.2f (Manufacture: %s%n)",product.getName(),product.getPrice(),sdf.format(((UsedProduct) product).getManufactureDate()));
+			}else {
+				System.out.printf("%s $ %.2f%n",product.getName(),product.getPrice());
+			}
+		}
+		sc.close();
 
 	}
 
