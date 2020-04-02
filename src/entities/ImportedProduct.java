5a401d8c9611fe7dd2fbe76d07eee38ed80c5204
Teogenes <teogenes.santos@gmail.com>
2020-04-02 17:07:47 -0300

    Implementacao da Classe ImportedProduct

--- src/entities/ImportedProduct.java
+++ src/entities/ImportedProduct.java
@@ -1,5 +1,29 @@
 package entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product{
+	private Double customesFee;
+	
+	public ImportedProduct() {
+		super();
+	}
 
+	public ImportedProduct(String name, Double price, Double customesFee) {
+		super(name, price);
+		this.customesFee = customesFee;
+	}
+
+	public Double getCustomesFee() {
+		return customesFee;
+	}
+
+	public void setCustomesFee(Double customesFee) {
+		this.customesFee = customesFee;
+	}
+	@Override
+	public String priceTag() {
+		return "teste";
+	}
+	public Double totalPrice() {
+		return 0.0;
+	}
 }
